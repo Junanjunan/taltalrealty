97193f75201ca2c5da09614a396b1c860e4f7cb3
taltal <taltalrealty@gmail.com>
2023-02-05 22:49:09 +0900

    Initialize home view with configuring template path

--- config/settings.py
+++ config/settings.py
@@ -40,7 +40,8 @@ DJANGO_APPS = [
 ]
 
 PROJECT_APPS = [
-    'users'
+    'users',
+    'home',
 ]
 
 THIRD_PARTY_APPS = []
@@ -63,7 +64,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
