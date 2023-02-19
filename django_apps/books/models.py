6c04aa0006c7e8c94870a01e2e3034212ede9d0e
taltal <taltalrealty@gmail.com>
2023-02-19 18:44:29 +0900

    Add tenant, tenant_phone field to Apartment, Room table

--- django_apps/books/models.py
+++ django_apps/books/models.py
@@ -4,9 +4,12 @@ from django_apps.books.common_models import *
 class Apartment(HouseCommon):
     floor = models.PositiveIntegerField(null=True, blank=True)
     empty = models.BooleanField(default=False)
+    tenant = models.CharField(max_length=100, null=True, blank=True)
+    tenant_phone = models.CharField(max_length=50, null=True, blank=True)
 
 
 class Room(HouseCommon):
     floor = models.PositiveIntegerField(null=True, blank=True)
     empty = models.BooleanField(default=False)
-    
+    tenant = models.CharField(max_length=100, null=True, blank=True)
+    tenant_phone = models.CharField(max_length=50, null=True, blank=True)
