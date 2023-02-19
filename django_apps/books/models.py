4b369b59c0ef84bfd7ee4aa867821ae5a88077a6
taltal <taltalrealty@gmail.com>
2023-02-19 22:02:15 +0900

    Update and Add some kinds of Books' table

--- django_apps/books/models.py
+++ django_apps/books/models.py
@@ -1,15 +1,28 @@
 from django_apps.books.common_models import *
 
 
-class Apartment(HouseCommon):
-    floor = models.PositiveIntegerField(null=True, blank=True)
-    empty = models.BooleanField(default=False)
-    tenant = models.CharField(max_length=100, null=True, blank=True)
-    tenant_phone = models.CharField(max_length=50, null=True, blank=True)
+class Apartment(HouseCommon, NotBuildingCommon):
+    pass
 
 
-class Room(HouseCommon):
-    floor = models.PositiveIntegerField(null=True, blank=True)
-    empty = models.BooleanField(default=False)
-    tenant = models.CharField(max_length=100, null=True, blank=True)
-    tenant_phone = models.CharField(max_length=50, null=True, blank=True)
+class Room(HouseCommon, NotBuildingCommon):
+    pass
+
+
+class Officetel(HouseCommon, NotBuildingCommon):
+    pass
+
+
+class Shop(BooksCommon, NotBuildingCommon):
+    premium = models.PositiveIntegerField(null=True, blank=True)
+
+
+class Building(BooksCommon):
+    under_floor = models.PositiveSmallIntegerField(null=True, blank=True)
+    land_type = models.CharField(max_length=30, null=True, blank=True)
+    area_building = models.FloatField(null=True, blank=True)
+    area_total_floor_area = models.FloatField(null=True, blank=True)
+    area_total_floor_area_for_ratio = models.FloatField(null=True, blank=True)
+    building_coverage = models.FloatField(null=True, blank=True)
+    floor_area_ratio = models.FloatField(null=True, blank=True)
+    parking_number = models.PositiveIntegerField(null=True, blank=True)
