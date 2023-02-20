9b17946921fa76a8dd4fa7fcc38f4392f09c4d2c
taltal <taltalrealty@gmail.com>
2023-02-21 03:01:28 +0900

    Set up STATICFILES_DIRS

--- config/settings.py
+++ config/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 from .taltalrealty_local_settings.local_settings import DJANGO_SECRET, DEBUG_CONFIG, POSTGRESQL
 
@@ -122,6 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
