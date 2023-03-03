714260bea7ea683e694f80abe598a836fd6e695d
taltal <taltalrealty@gmail.com>
2023-03-04 01:25:13 +0900

    Search count of rooms by using Q query of Django

--- django_apps/books/views.py
+++ django_apps/books/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from django.core.paginator import Paginator
 from django.apps import apps
+from django.db.models import Q
 from .models import *
 import importlib
 from .sort_list import table_list, sort_list, not_searching_list, contains_list, lte_list, gte_list, value_list, bool_list
@@ -11,11 +12,8 @@ def get_books_context(request, model, model_form):
     realtor = request.user.realtor_set.first()
     if realtor is None:
         return None
-    field_list = model._meta.get_fields()
-    field_list = sorted(field_list, key=lambda field: sort_list.index(field.name))
-    searching_field_list = [field for field in field_list if not field.name in not_searching_list]
-    table_field_list = [field.name for field in field_list if field.name in table_list]
-    filter_kwargs = {}
+
+    filter_kwargs = {'realtor':realtor}
     for k, v in request.GET.items():
         if v=='':
             continue
@@ -29,12 +27,28 @@ def get_books_context(request, model, model_form):
             filter_kwargs[k] = True
         elif k in value_list:
             filter_kwargs[k] = v
-    all_items = list(model.objects.filter(**filter_kwargs, realtor=realtor).values_list(*table_field_list).order_by('-updated'))
-    paginator = Paginator(all_items, 30)
+
+    room1 = request.GET.get("room1") == "on"
+    room2 = request.GET.get("room2") == "on"
+    room3 = request.GET.get("room3") == "on"
+    if room1 and room2:
+        query = Q(room=1)|Q(room=2)|Q(room__gte=3) if room3 else Q(room=1)|Q(room=2)
+    elif room1 and not room2:
+        query = Q(room=1)|Q(room__gte=3) if room3 else Q(room=1)
+    elif not room1 and room2:
+        query = Q(room=2)|Q(room__gte=3) if room3 else Q(room=2)
+    else:
+        query = Q(room__gte=3) if room3 else Q()
+    query = query&Q(**filter_kwargs)
+
+    field_list = sorted(model._meta.get_fields(), key=lambda field: sort_list.index(field.name))
+    searching_field_list = [field for field in field_list if not field.name in not_searching_list]
+    table_field_list = [field.name for field in field_list if field.name in table_list]
+    all_items = list(model.objects.filter(query).values_list(*table_field_list).order_by('-updated'))
+    paginator = Paginator(all_items, 5)
     page = request.GET.get('page', 1)
     page_items = paginator.get_page(page)
-    filtered_request = {k:v for k, v in request.GET.items() if v != ''}
-    model_form = model_form(initial=filtered_request)
+    model_form = model_form(initial=request.GET.items())
     context = {
         'searching_field_list': searching_field_list,
         'field_list': field_list,
